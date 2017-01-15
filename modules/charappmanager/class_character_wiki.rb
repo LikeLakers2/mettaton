f6627a80ebc20199ef36f367024c3022dc5522bc
LikeLakers2 <likelakers24@gmail.com>
2017-01-15 15:14:15 -0500

    Remove unneeded commands
    * Remove original `#search`
    * Remove `#search_not`
    * Rename `#search_op` to `#search`

--- modules/charappmanager/class_character_wiki.rb
+++ modules/charappmanager/class_character_wiki.rb
@@ -3,7 +3,7 @@ class CharacterDB < Wiki
 	# @param field [String] The field name to search
 	# @param op [Symbol] A symbol specifying what method to use to search
 	# @param text [Object] Text to search with.
-	def search_op(field, op, text, invert = false)
+	def search(field, op, text, invert = false)
 		@pages.select {|page|
 			next if page.nil?
 			page[field].send(op, text) unless invert
@@ -11,44 +11,27 @@ class CharacterDB < Wiki
 		}
 	end
 	
-	# Returns a new CharacterDB, trimmed down to the search results.
-	def search
-		@pages.select {|page|
-			next if page.nil?
-			yield page
-		}
-	end
-	
 	def search_equals(field, text)
-		search_op(field, :==, text)
+		search(field, :==, text)
 	end
 	
 	def search_contains(field, text)
-		search_op(field, :include?, text)
+		search(field, :include?, text)
 	end
 	
 	def search_regex(field, text)
-		search_op(field, :=~, text)
-	end
-	
-	
-	def search_not
-		s = search do |p|
-					yield page
-				end
-				
-		@pages.reject {|page| s.include? page}
+		search(field, :=~, text)
 	end
 	
 	def search_not_equals(field, text)
-		search_op(field, :==, text, true)
+		search(field, :==, text, true)
 	end
 	
 	def search_not_contains(field, text)
-		search_op(field, :include?, text, true)
+		search(field, :include?, text, true)
 	end
 	
 	def search_not_regex(field, text)
-		search_op(field, :=~, text, true)
+		search(field, :=~, text, true)
 	end
 end
