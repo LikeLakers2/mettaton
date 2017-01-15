510c22fda58230383cd1ccf075f974e213f398eb
LikeLakers2 <likelakers24@gmail.com>
2017-01-15 14:48:49 -0500

    Add a basic search method

--- modules/charappmanager/class_character_wiki.rb
+++ modules/charappmanager/class_character_wiki.rb
@@ -1,3 +1,27 @@
 class CharacterDB < Wiki
+	# Returns a new CharacterDB, trimmed down to the search results.
+	def search
+		@pages.select {|page|
+			next if page.nil?
+			yield page
+		}
+	end
 	
+	def search_equals(field, text)
+		search do |page|
+			page[field] == text
+		end
+	end
+	
+	def search_contains(field, text)
+		search do |page|
+			page[field].include? text
+		end
+	end
+	
+	def search_regex(field, text)
+		search do |page|
+			page[field] =~ text
+		end
+	end
 end
