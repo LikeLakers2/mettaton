1cb0aac9f559922d7b4433e1f6928050ebb382fa
LikeLakers2 <likelakers24@gmail.com>
2017-01-16 19:38:11 -0500

    Move search functions to wiki.rb + Start other functions

--- modules/charappmanager/class_character_wiki.rb
+++ modules/charappmanager/class_character_wiki.rb
@@ -1,46 +1,12 @@
 class CharacterDB < Wiki
-	#   _____ ______          _____   _____ _    _ 
-	#  / ____|  ____|   /\   |  __ \ / ____| |  | |
-	# | (___ | |__     /  \  | |__) | |    | |__| |
-	#  \___ \|  __|   / /\ \ |  _  /| |    |  __  |
-	#  ____) | |____ / ____ \| | \ \| |____| |  | |
-	# |_____/|______/_/    \_\_|  \_\\_____|_|  |_|
-	#
+	# __      _______ ________          __
+	# \ \    / /_   _|  ____\ \        / /
+	#  \ \  / /  | | | |__   \ \  /\  / / 
+	#   \ \/ /   | | |  __|   \ \/  \/ /  
+	#    \  /   _| |_| |____   \  /\  /   
+	#     \/   |_____|______|   \/  \/    
 	
-	# Search function
-	# @param field [String] The field name to search
-	# @param op [Symbol] A symbol specifying what method to use to search
-	# @param text [Object] Text to search with.
-	# @return [Array<Character>] The results that match your search.
-	def search(field, op, text, invert = false)
-		@pages.select {|page|
-			next if page.nil?
-			page[field].send(op, text) unless invert
-			!(page[field].send(op, text)) if invert
-		}
+	def view
+		
 	end
-	
-	def search_equals(field, text, invert = false)
-		search(field, :==, text, invert)
-	end
-	
-	def search_contains(field, text, invert = false)
-		search(field, :include?, text, invert)
-	end
-	
-	def search_regex(field, text, invert = false)
-		search(field, :=~, text, invert)
-	end
-	
-	#def search_not_equals(field, text)
-	#	search(field, :==, text, true)
-	#end
-	
-	#def search_not_contains(field, text)
-	#	search(field, :include?, text, true)
-	#end
-	
-	#def search_not_regex(field, text)
-	#	search(field, :=~, text, true)
-	#end
 end
--- modules/classes/wiki.rb
+++ modules/classes/wiki.rb
@@ -37,4 +37,38 @@ class Wiki
 		@pages.send(sym, *args)
 	end
 	
+	
+	
+	#   _____ ______          _____   _____ _    _ 
+	#  / ____|  ____|   /\   |  __ \ / ____| |  | |
+	# | (___ | |__     /  \  | |__) | |    | |__| |
+	#  \___ \|  __|   / /\ \ |  _  /| |    |  __  |
+	#  ____) | |____ / ____ \| | \ \| |____| |  | |
+	# |_____/|______/_/    \_\_|  \_\\_____|_|  |_|
+	#
+	
+	# Search function
+	# @param field [String] The field name to search
+	# @param op [Symbol] A symbol specifying what method to use to search
+	# @param text [Object] Text to search with.
+	# @return [Array<Character>] The results that match your search.
+	def search(field, op, text, invert = false)
+		@pages.select {|page|
+			next if page.nil?
+			page[field].send(op, text) unless invert
+			!(page[field].send(op, text)) if invert
+		}
+	end
+	
+	def search_equals(field, text, invert = false)
+		search(field, :==, text, invert)
+	end
+	
+	def search_contains(field, text, invert = false)
+		search(field, :include?, text, invert)
+	end
+	
+	def search_regex(field, text, invert = false)
+		search(field, :=~, text, invert)
+	end
 end
