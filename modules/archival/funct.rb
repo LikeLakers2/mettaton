396a3ee97cc2901d096e0b541479c73bd091b3cb
LikeLakers2 <likelakers24@gmail.com>
2017-01-30 23:22:34 -0500

    Archival update (#6)
    * Add files via upload
    
    * Commit archival to disk method
    
    * Start adding it alongside the original method
    
    * Variable name fixes, plus change archive_memory to start using the same format as archive_disk
    
    * empty logchannels.txt file
    
    * withids support start
    
    * concatenation
    
    * msg_to_string tweaks
    
    * tweaks and stuff
    
    * tweaks; remove unused methods; start organizing everything;
    
    * No longer need these two files
    
    * tweaks and fixes
    
    * Only allow admins to archive over 2000 messages
    
    * Allow queuing up of messages before we dump
    
    * Add configurable wait timer
    
    * tweaks

--- modules/archival/funct.rb
+++ modules/archival/funct.rb
@@ -1,57 +1,52 @@
 module ArchivalUnit
-	# Grabs all the messages we need and returns them in an array
-	# Return format: [[100,1],[200,101],[250,201]]
-	# 250 is earliest message, 1 is latest
-	def self.grab_history(event, count)
-		return [] if count <= 0
-		
-		history = []
-		#Discord only replies with 100 messages at max for each history request
-		fullgrabs = (count/100).floor
-		extragrab = count % 100
-		
-		before_id = nil
-		fullgrabs.times{|i|
-			history << event.channel.history(100, before_id)
-			
-			if history.last.empty? or history.last.length < 100 #or before_id == history.last.last.id
-				#Short-circuit if we notice that we aren't getting any more messages.
-				extragrab = 0
-				break
-			else
-				#Discord outputs latest messages first in the array
-				before_id = history.last.last.id
-				#So we don't spam the servers
-				sleep 0.1
-			end
-		}
-		
-		history << event.channel.history(extragrab, before_id) unless extragrab == 0
-		
-		# [[100,1],[200,101],[250,201]].reverse
-		# History array will be in a weird format so we need to reverse it
-		#history.reverse!
-		
-		return history
+	# Make this it's own method so I can update it easier during operation
+	def self.get_wait_time(x)
+		get_y_intercept(1, 0.1, 2000, 1, x)
+	end
+	def self.get_y_intercept(lowerlimit, lowerval, upperlimit, upperval, x)
+		# y - yA = ((yB - yA)/(xB - xA))*(x - xA)
+		# yB = lowerlimit   # yA = lowerval
+		# xB = upperlimit   # xA = upperval
+		# x = x
+		return upperval if x >= upperlimit
+		return lowerval if x <= lowerlimit
+		((lowerlimit - lowerval)/(upperlimit - upperval)) * (x - upperval) + lowerval
 	end
 	
-	# Changes the entire array from Message objects to String objects
-	# Return format: [250, 201, 200, 101, 100, 1]
-	# 250 is earliest message, 1 is latest
-	def self.history_to_text(history, withids = false)
-		text_ary = []
-		
-		history.each {|block|
-			#[250,201]
-			block.each {|msg|
-				text_ary << msg_to_string(msg, "", withids)
-			}
-		}
-		# [1, 100, 101, 200, 201, 250]
-		text_ary.reverse!
-		# [250, 201, 200, 101, 100, 1]
-		return text_ary
+	
+	def self.ary_to_hash(msg_ary)
+		msg_ary.map {|m| msg_to_hash(m)}
 	end
+	
+	def self.msg_to_hash(msg_obj)
+		{
+			:id => msg_obj.id,
+			:uid => msg_obj.author.id,
+			:content => msg_obj.content,
+			:attach => msg_obj.attachments.map {|attach| attach.url}.join(' ')
+		}
+	end
+	
+	def self.id_to_time(id)
+		ms = (id >> 22) + Discordrb::DISCORD_EPOCH
+		Time.at(ms / 1000.0)
+	end
+	
+	def self.json_files_to_log(event, file_ary)
+		log_ary = []
+		file_ary.each {|f|
+			log_ary += JSON.parse(File.read(f), symbolize_names: true)
+		}
+		
+		log_ary.sort! {|a,b|
+			a[:id] <=> b[:id]
+		}
+		
+		log_ary.map! {|m| yield m }
+		
+		save_log(event, log_ary)
+	end
+	
 	
 	# Takes the array of text and saves it to a log file, with some prepended text
 	# Return format: "FILENAME OF FILE.log"
@@ -71,7 +66,7 @@ module ArchivalUnit
 		
 		f.write(fparam[:introtext])
 		f.write(archive_array.join("\n"))
-		f.write("\n") # Closing newline so that 
+		f.write("\n")
 
 		f.close
 		
@@ -80,16 +75,15 @@ module ArchivalUnit
 	
 	
 	def self.filename_check(filename, ext)
-		return filename+ext unless File.file? filename+ext
+		return "#{filename}#{ext}" unless File.file? "#{filename}#{ext}"
 		
 		got_name = false
 		append_num = 1
-		filename_result = filename+"-"+append_num.to_s+ext
 		
-		until got_name do
+		until got_name
+			filename_result = "#{filename}-#{append_num}#{ext}"
 			if File.file? filename_result
 				append_num += 1
-				filename_result = filename+"-"+append_num.to_s+ext
 			else
 				got_name = true
 			end
