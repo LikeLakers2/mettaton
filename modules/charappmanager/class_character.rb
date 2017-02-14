2c1e7999cadba66f1254ab862712bc940ecd1aa0
LikeLakers2 <likelakers24@gmail.com>
2017-02-14 15:16:11 -0500

    move stuff around

--- modules/charappmanager/class_character.rb
+++ modules/charappmanager/class_character.rb
@@ -1,14 +1,10 @@
 class Character < PageWithProperties
 	def is_owner?(userid)
-		@properties["ownerid"] == userid
-	end
-	
-	def name
-		["name"] || "Unnamed Character" #Will change this to a proper attribute later
+		ownerid == userid.resolve_id
 	end
 	
 	def id
-		["charid"] #Will change this to a proper attribute later
+		["charid"]
 	end
 	alias_method :charid, :id
 	
@@ -16,41 +12,14 @@ class Character < PageWithProperties
 		["ownerid"] #Will change this to a proper attribute later
 	end
 	
+	def name
+		["name"] || "Unnamed Character"
+	end
+	
 	def load_from_json(file)
 		# TODO: JSON loading
 	end
 	
 	
-	# __      _______ ________          __
-	# \ \    / /_   _|  ____\ \        / /
-	#  \ \  / /  | | | |__   \ \  /\  / / 
-	#   \ \/ /   | | |  __|   \ \/  \/ /  
-	#    \  /   _| |_| |____   \  /\  /   
-	#     \/   |_____|______|   \/  \/    
-	#
-	
-	# Calls #view_props and #view_fields with the proc, and returns them joined by a newline
-	# @param &block [Proc] A proc that processes a key-value pair
-	# @return [String] All the results from the proc, joined by a single newline
-	def view(&block)
-		(view_props {|k,v| yield k,v}) + "\n" + (view_fields {|k,v| yield k,v})
-	end
-	
-	# Create output for a character, from @properties only
-	# @param &block [Proc] A proc that processes a key-value pair
-	# @return [String] All the results from the proc, joined by a single newline
-	def view_props(&block)
-		@properties.map {|k,v| yield k, v }.join("\n")
-	end
-	
-	# Create output for a character, from @fields only
-	# @param &block [Proc] A proc that processes a key-value pair
-	# @return [String] All the results from the proc, joined by a single newline
-	def view_fields(&block)
-		@fields.map {|k,v| yield k, v }.join("\n")
-	end
-	
-	def view_short(&block)
-		yield self
-	end
+
 end
--- modules/classes/pagewithproperties.rb
+++ modules/classes/pagewithproperties.rb
@@ -3,6 +3,12 @@ class PageWithProperties
 	######VARIABLES######
 	#####################
 	
+	# @return [Hash[String => Object]] Data for this page -- useful for internal stuff that normally shouldn't be touched.
+	#		{
+	#			"charid" => 123
+	#		}
+	attr_accessor :data
+	
 	# @return [Hash[String => Object]] A list of properties for this page
 	#		{
 	#			"ownerid" => 98296942768967680,
@@ -15,32 +21,49 @@ class PageWithProperties
 	#			"Name" => "Flowey, God of Hyperdeath",
 	#			"Age" => "???",
 	#			"Gender" => "He's a fucking flower",
-	#			"Appearance" => "http://i.imgur.com/Sw6aRCg.png https://i.imgur.com/VeHtcwql.png",
+	#			"Appearance" => "<http://i.imgur.com/Sw6aRCg.png> <https://i.imgur.com/VeHtcwql.png>",
 	#			"Brief Info" => "He hates people. He uses his vines to ~~fuck bees~~ entangle them and then choke a bitch to death. Also he's Asriel goddamn Dreemurr and he can transform into the God of Hyperdeath whenever he so chooses. He loves getting into fights and killing others.",
-	#			"Theme Song" => "https://www.youtube.com/watch?v=53WGSZVJFm4"
+	#			"Theme Song" => "<https://www.youtube.com/watch?v=53WGSZVJFm4>"
 	#		}
 	attr_accessor :fields
+	
 	
 	
 	#####################
 	#######METHODS#######
 	#####################
-	def initialize(properties = nil, fields = nil)
+	def initialize(properties = nil, fields = nil, data = nil)
+		@data = data || {}
 		@properties = properties || {}
 		@fields = fields || {}
 	end
 	
 	# Will grab from properties if it exists there, else grab from fields
 	def [](name)
-		if name == "properties" then @properties
-		elsif name == "fields" then @fields
-		else @properties[prop_get(name)] || @fields[field_get(name)]
+		case name.downcase
+		when "data" then @data
+		when "properties" then @properties
+		when "fields" then @fields
+		else @data[data_get(name)] || @properties[prop_get(name)] || @fields[field_get(name)]
 		end
 	end
 	
 	def get_key(name)
-		prop_get(name) || field_get(name)
+		data_get(name) || prop_get(name) || field_get(name)
 	end
+	
+	#-----#
+	
+	def data_get(name)
+		@data.select {|k,v| k.downcase == name.downcase }.keys.first
+	end
+	
+	def data_set!(name, value)
+		name = data_get(name) || name
+		@data[name] = value
+	end
+	
+	#-----#
 	
 	def prop_get(name)
 		@properties.select {|k,v| k.downcase == name.downcase }.keys.first
@@ -50,6 +73,8 @@ class PageWithProperties
 		name = prop_get(name) || name
 		@properties[name] = value
 	end
+	
+	#-----#
 	
 	def field_get(name)
 		@fields.select {|k,v| k.downcase == name.downcase }.keys.first
@@ -61,7 +86,42 @@ class PageWithProperties
 	end
 	
 	def key?(key)
-		@properties.key?[name] || @fields.key?[name] 
+		@data.key?(key) || @properties.key?(key) || @fields.key?(key)
+	end
+	
+	#---------------#
+	
+	# __      _______ ________          __
+	# \ \    / /_   _|  ____\ \        / /
+	#  \ \  / /  | | | |__   \ \  /\  / / 
+	#   \ \/ /   | | |  __|   \ \/  \/ /  
+	#    \  /   _| |_| |____   \  /\  /   
+	#     \/   |_____|______|   \/  \/    
+	#
+	
+	# Calls #view_props and #view_fields with the proc, and returns them joined by a newline
+	# @param &block [Proc] A proc that processes a key-value pair
+	# @return [String] All the results from the proc, joined by a single newline
+	def view(&block)
+		(view_props {|k,v| yield k,v}) + "\n" + (view_fields {|k,v| yield k,v})
+	end
+	
+	# Create output for a character, from @properties only
+	# @param &block [Proc] A proc that processes a key-value pair
+	# @return [String] All the results from the proc, joined by a single newline
+	def view_props(&block)
+		@properties.map {|k,v| yield k, v }.join("\n")
+	end
+	
+	# Create output for a character, from @fields only
+	# @param &block [Proc] A proc that processes a key-value pair
+	# @return [String] All the results from the proc, joined by a single newline
+	def view_fields(&block)
+		@fields.map {|k,v| yield k, v }.join("\n")
+	end
+	
+	def view_short(&block)
+		yield self
 	end
 	
 	#####################
@@ -69,6 +129,7 @@ class PageWithProperties
 	#####################
 	def to_json(*p)
 		{
+			"data" => @data,
 			"properties" => @properties,
 			"fields" => @fields
 		}.to_json(*p)
