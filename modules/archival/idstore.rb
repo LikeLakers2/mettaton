9dc26627e7bb0e072f7058f7ab7e82f64593aa81
LikeLakers2 <likelakers24@gmail.com>
2017-02-23 11:53:23 -0500

    cleanup of idstore.rb

--- modules/archival/idstore.rb
+++ modules/archival/idstore.rb
@@ -1,13 +1,4 @@
 module ArchivalUnit
-	# @return [Hash[Integer => String]] Snowflakes to server names
-	attr_accessor :servers
-	# @return [Hash[Integer => String]] Snowflakes to channel names
-	attr_accessor :channels
-	# @return [Hash[Integer => String]] Snowflakes to role names
-	attr_accessor :roles
-	# @return [Hash[Integer => String]] Snowflakes to user distincts
-	attr_accessor :users
-	
 	def self.idstore_update(bot)
 		h = idstore_hash(bot)
 		JSON.generate(h)
@@ -33,50 +24,5 @@ module ArchivalUnit
 			'roles'=>hr,
 			'users'=>hu
 		}
-	end
-	
-	#def self.idstore_hash_old(bot)
-	#	{
-	#		'servers'=>idu_servers(bot),
-	#		'channels'=>idu_channels(bot),
-	#		'roles'=>idu_roles(bot),
-	#		'users'=>idu_users(bot)
-	#	}
-	#end
-	
-	#def self.idu_servers(bot)
-	#	h = {}
-	#	bot.servers.each {|id, s|
-	#		h[id] = s.name
-	#	}
-	#	h
-	#end
-	
-	#def self.idu_channels(bot)
-	#	h = {}
-	#	bot.servers.each {|id, s|
-	#		s.channels.each {|c|
-	#			h[c.id] = c.name
-	#		}
-	#	}
-	#	h
-	#end
-	
-	#def self.idu_roles(bot)
-	#	h = {}
-	#	bot.servers.each {|id, s|
-	#		s.roles.each {|r|
-	#			h[r.id] = r.name
-	#		}
-	#	}
-	#	h
-	#end
-	
-	def self.idu_users(bot)
-		h = {}
-		bot.users.each {|id, u|
-			h[id] = u.distinct
-		}
-		h
 	end
 end
